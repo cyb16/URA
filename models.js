e8b8afd5598dc0e502223abd7acca17fcdaef53c
cyb16 <42992017+cyb16@users.noreply.github.com>
2023-11-27 13:25:59 +0300

    Add files via upload

--- models.js
+++ models.js
@@ -1,1 +1,82 @@
 //For models
+
+//model for taxpayer
+const mongoose = require('mongoose');
+
+const payerSchema = new mongoose.Schema({
+  payersName: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  annualIncome: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Payer', payerSchema);
+
+//model for assets
+const mongooseSequence = require('mongoose-sequence');
+
+const assetSchema = new mongoose.Schema({
+  assetName: {
+    type: String,
+    required: true,
+  },
+  estimatedCost: {
+    type: Number,
+    required: true,
+  },
+  ownerTIN: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  assetNumber: {
+    type: Number,
+    unique: true,
+  },
+});
+
+// Automatically generate asset number upon submission
+assetSchema.pre('save', async function (next) {
+  if (!this.assetNumber) {
+    const Asset = mongoose.model('Asset');
+    const assetNumber = await Asset.getNextSequenceValue();
+    this.assetNumber = assetNumber;
+  }
+  next();
+});
+
+// Configure mongoose-sequence for asset number generation
+mongooseSequence.configure({
+  name: 'asset_number_seq',
+  collectionName: 'counters',
+  inc: 1,
+});
+
+module.exports = mongoose.model('Asset', assetSchema);
--- server.js
+++ server.js
@@ -1,1 +1,62 @@
 //Express server here.
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const express = require('express');
+const app = express();
+const Payer  = require('./models');
+const Assets = require('./models');
+
+// MongoDB connection string
+const dbURI = "mongodb+srv://lifeofcyb:globetrotter@cluster0.mcrlzbg.mongodb.net/URA?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose.connect(dbURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB", error);
+  });
+
+// Function to insert the form data into the MongoDB collection
+async function insertFormData(formFields) {
+  try {
+    const payer = new Payer(formFields);
+    await payer.save();
+    console.log("Form data inserted successfully");
+  } catch (error) {
+    console.error("Error inserting form data:", error);
+  }
+}
+
+// Express middleware to handle the form submission
+async function handleFormSubmission(req, res) {
+  const formFields = {
+    payerName: req.body.payerName,
+    dateOfBirth: req.body.dateOfBirth,
+    occupation: req.body.occupation,
+    gender: req.body.gender,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    annualIncome: req.body.annualIncome,
+    tin: generateTIN(req.body.gender)
+  };
+
+  await insertFormData(formFields);
+
+  // Send a response to the client
+  res.send("Form submitted successfully");
+}
+
+// Function to generate the TIN
+function generateTIN(gender) {
+  const currentYear = new Date().getFullYear();
+  const tin = `${currentYear}/${gender.charAt(0).toUpperCase()}/${generateRandomNumber()}`;
+  return tin;
+}
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
